a8b9de56d56eb44bdae701219ea1a22eed5c4fc9
Anders Jurisoo <jurisoo@hotmail.com>
2018-10-09 18:34:23 +0200

    Added SegmentCollectionTest stub

---
+++ tests/Unit/SegmentCollectionTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Skeleton\SegmentCollection;
+
+class SegmentCollectionTest extends TestCase
+{
+    
+    /** @test */
+    public function file_runs()
+    {
+        $this->assertTrue(
+               true
+        );
+    }
+}
